979c7224d696d66e260d35985ec48ea152c5ff67
naoufalG <48034200+naoufalG@users.noreply.github.com>
2020-06-07 20:33:47 +0100

    Update entrepot.sql

--- entrepot.sql
+++ entrepot.sql
@@ -1,22 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 04, 2018 at 12:42 PM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.28-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `entrepot`
 --
 Create database Entrepot
@@ -76,11 +57,8 @@ create table Administrateur (
 
 create table Client (
  idClient int,
- idEntrepot int,
  nomClient varchar(254),
- prenomClient varchar(254),
- constraint PK_Client primary key (idClient)
- constraint FK_Client foreign key (idEntrepot)
+ prenomClient varchar(254)
 )
 
 -- --------------------------------------------------------
